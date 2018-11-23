a686e99188b8bdfe6652d0cba1825d9df2158644
Anabela Nogueira <nogueira.amt@gmail.com>
2018-11-23 00:32:49 +0000

    final update on server.js

--- server.js
+++ server.js
@@ -18,15 +18,67 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//US1: The API endpoint is GET [project_url]/api/timestamp/:date_string?
+app.route('/api/timestamp/:timestamp?')
+  .get(function (req, res){
+    
+    //US2: A date string is valid if can be successfully parsed by 
+    //new Date(date_string) (JS) . Note that the unix timestamp needs 
+    //to be an integer (not a string) specifying milliseconds. In our test 
+    //we will use date strings compliant with ISO-8601 (e.g. "2016-11-20") 
+    //because this will ensure an UTC timestamp.
+  
+    var date = null;
+    // parse the date string
+    if (req.params.timestamp !== undefined) {
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+      // check if it is a unix timestamp...
+      var unixTimestamp = parseInt(req.params.timestamp*1);
+      if (isNaN(unixTimestamp)) {
+        
+        // it's not a unix timestamp string
+        date = new Date(req.params.timestamp);
+      } else {
+        
+        // it is a timestamp
+        date = new Date(unixTimestamp);
+      }
+      
+    } else {
+      
+      //US3: If the date string is empty it should be equivalent to trigger 
+      //new Date(), i.e. the service uses the current timestamp.
+      // the date string parameter is empty. 
+      // create a new date based on current time 
+      date = new Date(Date.now());
+    }
+    //US4: If the date string is valid the api returns a JSON having the structure 
+    //{"unix": <date.getTime()>, "utc" : <date.toUTCString()> } e.g. 
+    //{"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}.
+    //US5: If the date string is invalid the api returns a JSON having the 
+    //structure {"unix": null, "utc" : "Invalid Date" }. It is what you get 
+    //from the date manipulation functions used above.
+    // Initialize the response object, if Date is invalid
+    // this one will be returned
 
+    var response = date == "Invalid Date" ? 
+      { error: "Invalid Date" } :
+      { "unix": date.getTime(),
+        "utc": date.toUTCString()
+      };
+    
+    res.json(response);
+  });
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// 404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
 });
